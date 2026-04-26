ac5fc3da9646eea7644ca96ca9e8fa55bd567d3d
Victor da Costa de Araujo Silva <dacostavictor123@gmail.com>
2026-04-26 15:50:44 -0300

    perguntas 1 e 2

---
+++ pergunta1.js
@@ -0,0 +1,82 @@
+localStorage.clear();
+
+class Personagem {
+  #nome;
+  #pontos;
+
+  constructor(nome, pontos) {
+    this.#nome = nome;
+    this.#pontos = pontos;
+  }
+
+  ganharPontos(qtd) {
+    this.#pontos += qtd;
+  }
+
+  getPontos() {
+    console.log(`pontuacao de ${this.#nome}: ${this.#pontos}`);
+    return this.#pontos;
+  }
+}
+
+const sonserina = new Personagem("Sonserina", 0);
+const gryffindor = new Personagem("Gryffindor", 0);
+const lufaLufa = new Personagem("Lufa Lufa", 0);
+const corvinal = new Personagem("Corvinal", 0);
+
+let qtd;
+
+const questao1 = document.getElementById("questao1");
+
+questao1.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const respostaSelecionada = document.querySelector(
+    'input[name="resposta"]:checked',
+  );
+
+  if (!respostaSelecionada) return;
+
+  const resposta1 = respostaSelecionada.value;
+
+  switch (resposta1) {
+    case "a":
+      qtd = 4;
+      lufaLufa.ganharPontos(qtd);
+      gryffindor.ganharPontos(--qtd);
+      corvinal.ganharPontos(--qtd);
+      sonserina.ganharPontos(--qtd);
+      break;
+
+    case "b":
+      qtd = 4;
+      gryffindor.ganharPontos(qtd);
+      lufaLufa.ganharPontos(--qtd);
+      sonserina.ganharPontos(--qtd);
+      corvinal.ganharPontos(--qtd);
+      break;
+
+    case "c":
+      qtd = 4;
+      corvinal.ganharPontos(qtd);
+      sonserina.ganharPontos(--qtd);
+      gryffindor.ganharPontos(--qtd);
+      lufaLufa.ganharPontos(--qtd);
+      break;
+
+    case "d":
+      qtd = 4;
+      sonserina.ganharPontos(qtd);
+      corvinal.ganharPontos(--qtd);
+      lufaLufa.ganharPontos(--qtd);
+      gryffindor.ganharPontos(--qtd);
+      break;
+  }
+
+  localStorage.setItem("sonserina", sonserina.getPontos());
+  localStorage.setItem("gryffindor", gryffindor.getPontos());
+  localStorage.setItem("lufaLufa", lufaLufa.getPontos());
+  localStorage.setItem("corvinal", corvinal.getPontos());
+
+  window.location.href = "pergunta2.html";
+});
